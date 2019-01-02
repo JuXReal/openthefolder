7764bf41e68b15b2ed7d40c61614466da47474f2
Sascha <Sascha.flach@googlemail.com>
2019-01-02 13:28:40 +0100

    Create openthefolder.ps1

---
+++ openthefolder.ps1
@@ -0,0 +1,19 @@
+#Creating a Loop 200 times.
+while ($i -lt 200)
+{
+$i++
+#Creating the GUI
+Add-Type -AssemblyName Microsoft.VisualBasic
+$number = [Microsoft.VisualBasic.Interaction]::InputBox('Enter Search String eg. Part of Foldername', 'Titel -OpenTheFolder', "$env:RMNumber")
+#Check if Searchfield is empty
+IF([string]::IsNullOrEmpty($number)) {            
+    Write-Host "Your string is EMPTY or NULL"
+    Exit           
+} else {          
+                    
+$number
+$pfad = Get-ChildItem "FOLDERPATH"  | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "$number"}
+ii $pfad.PSPath
+
+}
+}
